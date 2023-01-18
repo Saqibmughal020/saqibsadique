30212f54e2006f0841f1d48aa0cd42840d5329f8
Saqibmughal020 <102760544+Saqibmughal020@users.noreply.github.com>
2023-01-18 11:46:23 +0000

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,46 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+var checkDate = document.querySelector(".checkDate");
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+checkDate .innerHTML = dateTime;
+
+
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+calculate();
---
+++ js/calculation.js
@@ -0,0 +1,76 @@
+
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
+
+// for selecting different controls
+var search = document.querySelector(".searchBox");
+var convert = document.querySelector(".convert");
+var fromCurrecy = document.querySelector(".from");
+var toCurrecy = document.querySelector(".to");
+var finalValue = document.querySelector(".finalValue");
+var finalDate = document.querySelector(".finalDate");
+var From = document.querySelector(".From");
+var Too = document.querySelector(".too");
+var finalAmount = document.getElementById("finalAmount");
+var sel1 = document.getElementById("sel1");
+var sel2 = document.getElementById("sel2");
+var resultFrom;
+var resultTo;
+var searchValue;
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+// Event when currency is changed
+fromCurrecy.addEventListener('change', (event) => {
+	resultFrom = `${event.target.value}`;
+});
+
+// Event when currency is changed
+toCurrecy.addEventListener('change', (event) => {
+	resultTo = `${event.target.value}`;
+});
+
+search.addEventListener('input', updateValue);
+
+// function for updating value
+function updateValue(e) {
+	searchValue = e.target.value;
+}
+
+// when user clicks, it calls function getresults
+convert.addEventListener("click", getResults);
+
+// function getresults
+function getResults() {
+	fetch(`${api}`)
+		.then(currency => {
+			return currency.json();
+		}).then(displayResults);
+}
+
+// display results after conversion
+function displayResults(currency) {
+	let fromRate = currency.rates[resultFrom];
+	let toRate = currency.rates[resultTo];
+	finalValue.innerHTML =
+	((toRate / fromRate) * searchValue).toFixed(2);
+	finalAmount.style.display = "block";
+    // alert(sel1.value)
+    
+ finalDate .innerHTML = dateTime;
+ From .innerHTML = sel1.value;
+ Too .innerHTML = sel2.value;
+ 
+}
+
+// when user click on reset button
+function clearVal() {
+	window.location.reload();
+	document.getElementsByClassName("finalValue").innerHTML = "";
+};
+
+
+
+
+
+console.log(dateTime)
